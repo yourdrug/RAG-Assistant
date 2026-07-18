f26f4c9132b9fd29d79c3cf9bfe7c2337a0c78ad
eugene <mitkojenia@gmail.com>
2026-07-18 18:14:43 +0300

    add pre-commit & github pipelines & license

--- server/app/main.py
+++ server/app/main.py
@@ -88,7 +88,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ограничь конкретным доменом в продакшне
+    allow_origins=settings.allowed_origins_list,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
