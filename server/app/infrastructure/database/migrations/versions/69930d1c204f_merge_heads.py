b48123879752a6107b2b4b2dd69e3290184ae1e8
eugene <mitko@ivcmf.by>
2026-08-14 17:57:56 +0300

    fix: merge alembic heads

---
+++ server/app/infrastructure/database/migrations/versions/69930d1c204f_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: 69930d1c204f
+Revises: b2c3d4e5f6a8, j5k6l7m8n9o0
+Create Date: 2026-08-14 17:56:32.687772
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '69930d1c204f'
+down_revision: Union[str, Sequence[str], None] = ('b2c3d4e5f6a8', 'j5k6l7m8n9o0')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
